ee394e6df944822c699d226f4195ccfcac6747c4
manasjain1912 <manasjainsgr@gmail.com>
2023-04-02 01:42:09 +0530

    added dashboard

---
+++ dashboard/src/pages/AboutUs.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+export const AboutUs = () => {
+return (
+	<div className="home">
+	<h6>Welcome to Jaundice Detection System</h6>
+	</div>
+);
+};
+
+export const OurAim = () => {
+return (
+	<div className="home">
+	<h6>Aim</h6>
+	</div>
+);
+};
+
+export const OurVision = () => {
+return (
+	<div className="home">
+	<h6>Vision</h6>
+	</div>
+);
+};
